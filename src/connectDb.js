a3cddb314a901488938ddf07be4f4d4fbb3ff84c
Ashley Dennis <adenn1852@gmail.com>
2022-03-06 15:59:35 -0500

    first push

---
+++ src/connectDb.js
@@ -0,0 +1,10 @@
+const {initializeApp, cert} = require('firebase-admin/app')
+const {getFirestore} =require ('firebase-admin/firestore')
+
+const credentials = require('../credentials.json')
+
+initializeApp({
+    credential: cert(credentials)
+})
+
+const db = getFirestore()
