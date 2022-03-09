a0aecb6aa1d0863b13334ed5b923915492fccd30
Ashley Dennis <adenn1852@gmail.com>
2022-03-08 23:02:07 -0500

    added cors and deploy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors")
 const { createVibe, getRestaurants, getParty, getChill, getChic } = require("./restaurants");
 
 const port = 3000;
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.post("/restaurants", createVibe);
 
