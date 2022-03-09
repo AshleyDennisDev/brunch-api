d6c9fc858fea85922f5c50247fc0c522cda2782f
Ashley Dennis <adenn1852@gmail.com>
2022-03-09 13:09:55 -0500

    successful deploy to gcloud

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors")
 const { createVibe, getRestaurants, getParty, getChill, getChic } = require("./restaurants");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
